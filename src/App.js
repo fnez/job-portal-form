6acf820ff6115e5224459630e96189b78d9077d7
Froi Nunez <froinunez@Frois-MBP.attlocal.net>
2024-11-08 21:38:09 -0500

    Adding typescript, remove unused logos, adding react router-dom

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { LoginForm } from "./Components/LoginForm/LoginForm";
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([]);
 
 function App() {
   return (
